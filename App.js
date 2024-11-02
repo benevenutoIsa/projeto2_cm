2595b82a617205a54ce50643a02ade90515d3911
benevenutoIsa <142352312+benevenutoIsa@users.noreply.github.com>
2024-11-01 22:19:16 -0300

    Update App.js
    função de cadastrar (usando armazenamento local)

--- App.js
+++ App.js
@@ -10,3 +10,44 @@ import styles from './styles';
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
+function PaginaCadastro() {
+  const [usuario, setUsuario] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleRegister = async () => {
+    if (usuario && senha) {
+      try {
+        await AsyncStorage.setItem(usuario, senha);
+        Alert.alert('Sucesso', 'Usuário cadastrado!');
+        setUsuario('');
+        setSenha('');
+      } catch (error) {
+        Alert.alert('Erro');
+      }
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>organiZer</Text>
+      <Text style={styles.subText}>O app que irá te ajudar a organizar sua rotina!</Text>
+      <Text style={styles.subText}>Cadastre-se para começar sua jornada</Text>
+      
+      <Text style={styles.title}>Cadastro</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="crie um nome de usuário"
+        value={usuario}
+        onChangeText={setUsuario}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="crie uma senha"
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry
+      />
+      <Button title="Registrar" onPress={handleRegister} />
+    </SafeAreaView>
+  );
+}

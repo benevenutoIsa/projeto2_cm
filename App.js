27634939cfcf28211e65e933fb252c300a50014e
benevenutoIsa <142352312+benevenutoIsa@users.noreply.github.com>
2024-11-01 22:06:11 -0300

    Update App.js
    cria e configura os navegadores que deixam o app organizado

--- App.js
+++ App.js
@@ -6,3 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import styles from './styles';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+

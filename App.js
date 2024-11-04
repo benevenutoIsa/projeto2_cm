03ae7af0a76bced0e81eb13a033bb3625eb0e5d7
benevenutoIsa <142352312+benevenutoIsa@users.noreply.github.com>
2024-11-03 22:53:24 -0300

    Update App.js
    implementação da pagina de login

--- App.js
+++ App.js
@@ -1,38 +1,40 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Text, TextInput, Button, Alert, TouchableOpacity, FlatList, View 
-} from 'react-native';
+import React, { useState, useEffect  } from 'react';
+import { SafeAreaView, Text, TextInput, Button, Alert, TouchableOpacity, FlatList, View, Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Audio } from 'expo-av';
 import styles from './styles';
 
+//cria navegadores das paginas
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
+//função de cadastrar o usuario
 function PaginaCadastro() {
+  //onde vão ser armazenados os valores de usuario e senha
   const [usuario, setUsuario] = useState('');
   const [senha, setSenha] = useState('');
-
-  const handleRegister = async () => {
+  const handleRegistrar = async () => {
+    //guarda no armazenamento local, deixa o campo vazio novamente 
     if (usuario && senha) {
       try {
         await AsyncStorage.setItem(usuario, senha);
         Alert.alert('Sucesso', 'Usuário cadastrado!');
         setUsuario('');
         setSenha('');
-      } catch (error) {
+      } catch (erro) {
         Alert.alert('Erro');
       }
     }
   };
-
+  //monta a tela de cadastro com conexão ao arquivo de style css
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.appTitle}>organiZer</Text>
       <Text style={styles.subText}>O app que irá te ajudar a organizar sua rotina!</Text>
       <Text style={styles.subText}>Cadastre-se para começar sua jornada</Text>
-      
       <Text style={styles.title}>Cadastro</Text>
       <TextInput
         style={styles.input}
@@ -47,7 +49,68 @@ function PaginaCadastro() {
         onChangeText={setSenha}
         secureTextEntry
       />
-      <Button title="Registrar" onPress={handleRegister} />
+      <Button title="Registrar" onPress={handleRegistrar} />
     </SafeAreaView>
   );
 }
+
+//função da pagina de login
+function PaginaLogin({ navigation }) {
+  //onde vão ser armazenados os valores de usuario e senha
+  const [usuario, setUsuario] = useState('');
+  const [senha, setSenha] = useState('');
+  const [sound, setSound] = useState();
+
+  // carregar o som para quando o login for efetuado com sucesso
+  const loadSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require('./assets/login-success.mp3') 
+    );
+    setSound(sound);
+  };
+  //faz a confirmação se o login foi realizado corretamente e solta o som escolhido 
+  const handleLogin = async () => {
+    const senhaArmazenada = await AsyncStorage.getItem(usuario);
+    if (senhaArmazenada === senha) {
+      Alert.alert('Sucesso', 'Login realizado com sucesso!');
+      if (sound) {
+        await sound.replayAsync(); 
+      }
+      navigation.navigate('Filmes');
+    } else {
+      Alert.alert('Erro', 'Usuário ou senha incorretos.');
+    }
+  };
+  
+  useEffect(() => {
+    loadSound(); //solta o som ao entrar na pagina depois de confirma
+    return sound
+      ? () => {
+          sound.unloadAsync(); //para ele
+        }
+      : undefined;
+  }, []);
+//monta a tela de cadastro com conexão ao arquivo de style css
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>organiZer</Text>
+      <Text style={styles.subText}>Insira seu login para continuar</Text>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome de usuário"
+        value={usuario}
+        onChangeText={setUsuario}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite sua senha"
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry
+      />
+      <Button title="Entrar" onPress={handleLogin} />
+    </SafeAreaView>
+  );
+}
+
